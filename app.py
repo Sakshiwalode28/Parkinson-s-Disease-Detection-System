8c28139c787ded4817ec888ae36697b3c4841923
Sakshi Walode <79333447+Sakshiwalode28@users.noreply.github.com>
2023-04-03 03:50:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ if (selected == "Parkinsons Test"):
     st.title("Parkinson's Disease Detection Test")
     col1, col2,col3, col4 = st.columns(4)  
   
-    i = 1000
+    i = 0
 #     if(screenWidth <= 768  ):
     if(i <= 768  ):
         fo = st.text_input('MDVP: Fo(Hz)')
